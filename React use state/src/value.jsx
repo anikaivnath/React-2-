f323cde5fca74328f69c73a885ed87cbf95710b4
anikaivnath <anikaivnath8@gmail.com>
2024-09-13 02:37:40 +0600

    set function

--- React use state/src/value.jsx
+++ React use state/src/value.jsx
@@ -2,12 +2,22 @@
 import { useState } from 'react';
 
 const Value = ()=> {
+const[ value,setValue]=useState(10);
+
+const increase =()=>{
+  setValue(value+1)
+};
+const decrease =()=>{
+  setValue(value-1)
+};
+
 
 return (
     <>
     <div className=''>
-    <button>Increase+1</button>
-    <button>Decrease -1</button>
+    <button onClick={increase}>Increase+1</button>
+    <button onClick={decrease}>Decrease -1</button>
+    <p>Value is {value}</p>
 
     </div>
 
